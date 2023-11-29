1162db853ebfee519bd5f7b21c6049bc01b31b14
Daniel Keller <daniel@pugcode.works>
2023-11-29 13:48:45 -0800

    fix typos in examples

--- src/test/scala/pug/schema/Examples.scala
+++ src/test/scala/pug/schema/Examples.scala
@@ -49,7 +49,6 @@ object Examples {
   /* Arg!!! In some deployments, cats were assigned an ennui of 0, breaking our migration from
    * version 1 -> 2. Fortunately, the schema manager will apply the migration which advances the
    * version as far as possible, which we can use to advance past the problematic migration.
-   * and we'd like to add some information to reflect that.
    */
   object Step3 {
     object PetsSchema extends SchemaComponent("pets") {
@@ -77,7 +76,7 @@ object Examples {
           ALTER TABLE cats ADD CHECK (frolic_factor > 0);
         """
 
-        // If we have not been able to advance from version 1, so some additional fixup,
+        // If we have not been able to advance from version 1, do some additional fixup,
         // then apply those steps.
         case (1, 3) =>
           sql"""UPDATE cats SET ennui = CASE WHEN ennui <= 0 THEN null ELSE ennui END""" ++
